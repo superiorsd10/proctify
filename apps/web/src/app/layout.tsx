a204d0d67d96229911625e942cab28a76115db35
superiorsd10 <sachindapkara6@gmail.com>
2024-11-20 15:00:33 +0530

    feat(web:app): use Toaster in root layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,8 @@
-import "./globals.css";
+import "@repo/ui/globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "@repo/ui/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
