f2d27cb3a7b956716210125afb9b017af5c7debf
superiorsd10 <sachindapkara6@gmail.com>
2024-11-20 17:49:25 +0530

    feat(web:app): add a new folder constants, and detectionConstants inside it

---
+++ apps/web/src/app/constants/detectionConstants.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_PROHIBITED_OBJECTS = [
+  "cell phone",
+  "mobile phone",
+  "phone",
+  "laptop",
+  "tablet",
+  "computer",
+];
+
+export const PERSON_ABSENCE_THRESHOLD = 6000;
+export const DEFAULT_OBJECT_THRESHOLD = 0.4;
+export const DEFAULT_NOISE_THRESHOLD = 60;
