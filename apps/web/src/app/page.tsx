9471355321dae33a424891c3a7555b657dfcaeb8
superiorsd10 <sachindapkara6@gmail.com>
2024-11-21 16:06:40 +0530

    feat(web:src): modify homepage to show create and join test buttons

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import { Button } from "@repo/ui/components/ui/button";
 
-export default function Page(): JSX.Element {
+export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <Button>Welcome to proctify</Button>
-    </main>
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <div className="space-x-4">
+        <Link href="/create-test">
+          <Button variant="default">Create Test</Button>
+        </Link>
+        <Button variant="outline">Join Test</Button>
+      </div>
+    </div>
   );
 }
