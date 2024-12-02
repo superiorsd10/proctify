55d722810c3536bf06bd274009676b3b46c0bbb0
superiorsd10 <sachindapkara6@gmail.com>
2024-12-02 18:28:36 +0530

    feat(server:config): add new config vars

--- apps/server/src/config/config.ts
+++ apps/server/src/config/config.ts
@@ -4,6 +4,12 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT || 3001,
+  nodeEnv: process.env.NODE_ENV || "development",
+  redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
+  kafkaBroker: process.env.KAFKA_BROKER || "localhost:9092",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION || "us-east-1",
 };
 
 export default config;
