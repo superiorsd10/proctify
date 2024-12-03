226111944db46c0cc55868cf6a9cb9afa17978a5
superiorsd10 <sachindapkara6@gmail.com>
2024-12-03 18:18:54 +0530

    feat(server:routes): use the validation middleware in create test route

--- apps/server/src/routes/testRoutes.ts
+++ apps/server/src/routes/testRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { TestController } from "../controllers/TestController";
+import { validationMiddleware } from "@repo/middlewares";
+import { createTestSchema } from "@repo/validation";
 
 const router = express.Router();
 const testController = new TestController();
 
-router.post("/", testController.createTest.bind(testController));
+router.post(
+  "/",
+  validationMiddleware(createTestSchema),
+  testController.createTest.bind(testController)
+);
 
 export default router;
