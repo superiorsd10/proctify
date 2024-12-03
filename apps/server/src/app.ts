f22b58ad94a6dcedd746716db899b7e04b3ae3bf
superiorsd10 <sachindapkara6@gmail.com>
2024-12-03 12:00:05 +0530

    feat(server:app): use rate limiter middleware and error handler middleware in create app function

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,14 +2,28 @@ import express, { Express } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import {
+  createRateLimiterMiddleware,
+  errorHandlerMiddleware,
+} from "@repo/middlewares";
+import { RedisService } from "@repo/services";
 
 export default function createApp(): Express {
   const app = express();
+
+  const redisService = RedisService.getInstance();
+  const rateLimiterMiddleware = createRateLimiterMiddleware(redisService);
 
   app.use(helmet());
   app.use(morgan("dev"));
   app.use(cors());
   app.use(express.json());
+
+  app.use(rateLimiterMiddleware);
+
+  // routes
+
+  app.use(errorHandlerMiddleware);
 
   return app;
 }
