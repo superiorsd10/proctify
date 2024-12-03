15747f488ae6e29fd9311468c77b319c30a5e765
superiorsd10 <sachindapkara6@gmail.com>
2024-12-03 18:08:55 +0530

    feat(middlewares:src): update the error handler middleware message

--- packages/middlewares/src/errorHandlerMiddleware.ts
+++ packages/middlewares/src/errorHandlerMiddleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import { ApiError } from "@repo/utils";
 import { HttpStatusCode } from "@repo/utils";
 
-export const errorHandlerMiddleware = (
+export const errorHandlerMiddleware: ErrorRequestHandler = (
   err: Error,
   req: Request,
   res: Response,
@@ -11,11 +11,12 @@ export const errorHandlerMiddleware = (
   console.error(err);
 
   if (err instanceof ApiError) {
-    return res.status(err.code).json({ error: err.message, success: false });
+    res.status(err.code).json({ error: err.message, success: false });
+    return;
   }
 
-  return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
-    error: "Internal Server Error",
+  res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+    error: err.message,
     success: false,
   });
 };
