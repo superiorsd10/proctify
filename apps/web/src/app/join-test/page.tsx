a253375597f462ce2f7932c06679fd3285d2c9d6
superiorsd10 <sachindapkara6@gmail.com>
2024-12-03 20:46:24 +0530

    feat(web:app): handle the submit on joining the test in join-test page

--- apps/web/src/app/join-test/page.tsx
+++ apps/web/src/app/join-test/page.tsx
@@ -13,17 +13,72 @@ import {
   CardContent,
   CardFooter,
 } from "@repo/ui/components/ui/card";
+import { useAuth } from "@clerk/nextjs";
+import { useToast } from "@repo/ui/components/hooks/use-toast";
+import { SERVER_BASE_URL } from "src/constants/configurationConstants";
 
 export default function JoinTest() {
   const router = useRouter();
   const [code, setCode] = useState("");
 
+  const { toast } = useToast();
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    //TODO: send the log data to your backend
     console.log({ code });
-    //TODO: Navigate to test page after joining
-    router.push("/");
+
+    const { userId } = useAuth();
+
+    if (!userId) {
+      toast({
+        variant: "destructive",
+        title: "User Not Authenticated",
+        description: "You must be logged in to join a test.",
+      });
+      return;
+    }
+
+    const testData = {
+      testId: code,
+      userId: userId,
+    };
+
+    try {
+      const response = await fetch(`${SERVER_BASE_URL}/test/join`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(testData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        toast({
+          variant: "success",
+          title: "Joined Test Successfully",
+          description: data.message || "You have successfully joined the test.",
+        });
+
+        router.push("/");
+      } else {
+        const errorData = await response.json();
+
+        toast({
+          variant: "destructive",
+          title: "Error Joining Test",
+          description:
+            errorData.message || "There was an error joining the test.",
+        });
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Unexpected Error",
+        description: "An unexpected error occurred. Please try again later.",
+      });
+    }
   };
 
   return (
