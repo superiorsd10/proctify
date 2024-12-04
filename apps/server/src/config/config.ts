6b53f108d504f150771604606438d3fa3a63933a
superiorsd10 <sachindapkara6@gmail.com>
2024-12-04 18:40:58 +0530

    feat(server:config): add aws s3 bucket name to config

--- apps/server/src/config/config.ts
+++ apps/server/src/config/config.ts
@@ -10,7 +10,7 @@ const config = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION || "us-east-1",
-  bucketName: process.env.AWS_BUCKET_NAME,
+  bucketName: process.env.AWS_S3_BUCKET_NAME,
 };
 
 export default config;
