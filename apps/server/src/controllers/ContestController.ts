56464dcc493b0ed1460e5a6984ab8f6b966b3473
superiorsd10 <sachindapkara6@gmail.com>
2024-12-05 00:47:33 +0530

    feat(server:controllers): add join contest and fetch contest logs method to contest controller

--- apps/server/src/controllers/ContestController.ts
+++ apps/server/src/controllers/ContestController.ts
@@ -106,4 +106,138 @@ export class ContestController {
       next(error);
     }
   }
+
+  async joinContest(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { contestId, userId } = req.body;
+
+      const contest = await prisma.contest.findUnique({
+        where: { id: contestId },
+      });
+      if (!contest) {
+        res
+          .status(HttpStatusCode.NOT_FOUND)
+          .json({ success: false, message: "Contest not found" });
+        return;
+      }
+
+      const existingContestLog = await prisma.contestLog.findFirst({
+        where: { contestId, userId },
+      });
+
+      if (existingContestLog) {
+        res.status(HttpStatusCode.CREATED).json({
+          success: true,
+          message: "User successfully joined the contest",
+          data: existingContestLog,
+          startTime: contest.startTime,
+        });
+        return;
+      }
+
+      const contestLog = await prisma.contestLog.create({
+        data: {
+          contestId,
+          userId,
+          audioViolations: 0,
+          noFaceViolations: 0,
+          multipleFaceViolations: 0,
+          keypressViolations: 0,
+          rightClickViolations: 0,
+          windowChangeViolations: 0,
+          prohibitedObjectViolations: 0,
+          ufmScore: 0,
+        },
+      });
+
+      res.status(HttpStatusCode.CREATED).json({
+        success: true,
+        message: "User successfully joined the contest",
+        data: contestLog,
+        startTime: contest.startTime,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async fetchContestLogs(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { contestId, userId, page = 1 } = req.query;
+
+      if (!contestId || !userId) {
+        res.status(HttpStatusCode.BAD_REQUEST).json({
+          success: false,
+          message: "contestId and userId are required",
+        });
+        return;
+      }
+
+      const contest = await prisma.contest.findUnique({
+        where: { id: contestId as string },
+      });
+
+      if (!contest) {
+        res.status(HttpStatusCode.NOT_FOUND).json({
+          success: false,
+          message: "Contest not found",
+        });
+        return;
+      }
+
+      if (contest.userId !== userId) {
+        res.status(HttpStatusCode.FORBIDDEN).json({
+          success: false,
+          message: "You are not authorized to view these contest logs",
+        });
+        return;
+      }
+
+      const pageNumber = parseInt(page as string, 10);
+      const skip = (pageNumber - 1) * 10;
+
+      const contestLogs = await prisma.contestLog.findMany({
+        where: {
+          contestId: contestId as string,
+        },
+        orderBy: {
+          ufmScore: "desc",
+        },
+        skip,
+        take: 10,
+        include: {
+          user: {
+            select: {
+              username: true,
+            },
+          },
+        },
+      });
+
+      const totalContestLogs = await prisma.contestLog.count({
+        where: {
+          contestId: contestId as string,
+        },
+      });
+
+      const totalPages = Math.ceil(totalContestLogs / 10);
+
+      res.status(HttpStatusCode.SUCCESS).json({
+        success: true,
+        message: "Fetched contest logs successfully",
+        data: contestLogs.map((contestLog) => ({
+          ...contestLog,
+          username: contestLog.user?.username, 
+        })),
+        title: contest.title,
+        pagination: {
+          currentPage: pageNumber,
+          totalPages,
+          totalContestLogs,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
